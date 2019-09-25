052356ba30ef1e51e57c6cf3e7b015ad2fd2d38f
SRoller <sroller@justfab.com>
2019-09-24 22:26:12 -0400

    adds more snippets

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -27,3 +27,35 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+
+// When to use annotations
+// 1) function that returns the 'any' types
+const json = '{ "x": 10, "y": 20 }';
+const coordinates = JSON.parse(json);
+const coordinateWithType: { x: number, y: number }= JSON.parse(json);
+console.log(coordinateWithType.x);
+
+// When we declare a variable on one line
+// and initialize it later
+let words = ['red', 'green', 'blue'];
+let foundWord;
+let typeFoundWord: Boolean;
+
+for (let i = 0; i < words.length; i++) {
+  const word = words[i];
+  if (word === 'green') {
+    foundWord = true;
+    typeFoundWord = true;
+  }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
